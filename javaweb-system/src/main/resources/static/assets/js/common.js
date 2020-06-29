dcc000ecac5c3260e06f87b3cb12bfdef5ffea24
牧羊人 <1175401194@qq.com>
2020-06-29 09:15:49 +0800

    优化底层功能

--- javaweb-system/src/main/resources/static/assets/js/common.js
+++ javaweb-system/src/main/resources/static/assets/js/common.js
@@ -1,4 +1,4 @@
-/** EasyWeb iframe v3.1.8 date:2020-05-04 License By http://easyweb.vip */
+/** JavaWeb License By http://www.javaweb.vip */
 layui.config({  // common.js是配置layui扩展模块的目录，每个页面都需要引入
     version: '318',   // 更新组件缓存，设为true不缓存，也可以设一个固定值
     base: getProjectUrl() + 'assets/module/'
